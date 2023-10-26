b2210b42a68d38909dfc5b36bb3755d91c55d965
Andreas054 <84636763+Andreas054@users.noreply.github.com>
2023-10-26 19:47:45 +0300

    lab3 - neterminat

---
+++ DAW-Lab3/DAW-Lab3/Controllers/DatabaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAW_Lab3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseController : ControllerBase
+    {
+    }
+}
